c3f8220668162c8b209ce5a171c8aa4ee7bd1b60
kiv1n <aloha@kiv1n.ru>
2024-01-31 18:30:09 +0300

    Implement landing page

---
+++ app/components/site-header.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { siteConfig } from "@/config/site";
+import { Github } from "lucide-react";
+import Link from "next/link";
+import { ThemeToggle } from "./theme-toggle";
+import { buttonVariants } from "./ui/button";
+
+export function SiteHeader() {
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        <div className="flex gap-6 md:gap-10">
+          <Link href="/" className="flex items-center space-x-2">
+            <span className="inline-block text-primary font-bold">
+              {siteConfig.emoji}{" "}
+              <span className="hidden md:inline-block">{siteConfig.name}</span>
+            </span>
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <nav className="flex items-center space-x-1">
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={buttonVariants({
+                  size: "icon",
+                  variant: "ghost",
+                })}
+              >
+                <Github className="h-5 w-5" />
+                <span className="sr-only">GitHub</span>
+              </div>
+            </Link>
+            <ThemeToggle />
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ app/config/site.ts
@@ -0,0 +1,10 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  emoji: "🏆",
+  name: "XP Tournaments",
+  description: "Launch tournaments, beat opponents, earn XP",
+  links: {
+    github: "https://github.com/web3goals/xp-tournaments-prototype",
+  },
+};
